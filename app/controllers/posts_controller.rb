1c37efd39ab2773ad861318f431d052a799a3ad9
okamayu <mayu.okagawa0511@gmail.com>
2020-07-19 12:21:41 +0900

    add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(:user).order("created_at DESC")
+    @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
